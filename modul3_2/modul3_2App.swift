4e5c9052aa580dc383062e7e37c0bf970947c856
Iuliia Morozova <yulii.morozova@gmail.com>
2026-02-20 14:08:16 +0100

    Initial Commit

---
+++ modul3_2/modul3_2App.swift
@@ -0,0 +1,17 @@
+//
+//  modul3_2App.swift
+//  modul3_2
+//
+//  Created by Julia Morozova on 20. 2. 2026..
+//
+
+import SwiftUI
+
+@main
+struct modul3_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
